60f77d259b3602521ebdec2eb45d754e9ff0ea0b
jdi022222 <jdi022222@gmail.com>
2024-02-13 20:39:51 +0900

    initial commit

---
+++ src/main/java/com/example/hexagonalarchitecturesample/HexagonalArchitectureSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexagonalarchitecturesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchitectureSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hexagonalarchitecturesample/HexagonalArchitectureSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonalarchitecturesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
